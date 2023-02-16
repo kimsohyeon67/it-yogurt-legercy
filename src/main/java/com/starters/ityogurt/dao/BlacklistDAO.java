a39857667b527e9cc108b7b14249d6683e519694
HK <sweetapplepie153@gmail.com>
2023-02-16 15:29:43 +0900

    Refactor : 필요없는 import 삭제

--- src/main/java/com/starters/ityogurt/dao/BlacklistDAO.java
+++ src/main/java/com/starters/ityogurt/dao/BlacklistDAO.java
@@ -1,11 +1,9 @@
 package com.starters.ityogurt.dao;
 
-import java.util.List;
 
 import org.apache.ibatis.annotations.Mapper;
 import org.springframework.stereotype.Repository;
 
-import com.starters.ityogurt.dto.UserDTO;
 
 @Mapper
 @Repository
--- src/main/java/com/starters/ityogurt/service/BlacklistService.java
+++ src/main/java/com/starters/ityogurt/service/BlacklistService.java
@@ -1,8 +1,5 @@
 package com.starters.ityogurt.service;
 
-import java.util.List;
-
-import com.starters.ityogurt.dto.UserDTO;
 
 public interface BlacklistService {
 	
