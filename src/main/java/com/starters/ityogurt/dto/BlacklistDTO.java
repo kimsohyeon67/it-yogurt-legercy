77c0f9d6fa8d68e6fc5e3062f000f76db9fe85ef
HK <sweetapplepie153@gmail.com>
2023-02-16 15:30:47 +0900

    Refactor : blacklistDTO lombok 적용

--- src/main/java/com/starters/ityogurt/dto/BlacklistDTO.java
+++ src/main/java/com/starters/ityogurt/dto/BlacklistDTO.java
@@ -1,20 +1,16 @@
 package com.starters.ityogurt.dto;
 
-import java.util.Date;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.stereotype.Component;
 
+
+
+@Getter
+@Setter
+@Component
 public class BlacklistDTO { 
 	
 	String email;
-
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
 	
 }
