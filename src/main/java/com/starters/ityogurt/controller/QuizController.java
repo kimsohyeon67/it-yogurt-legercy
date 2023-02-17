25f8d939a60bcfa7253fc232b2671b1e8317ac9c
thfastang401 <thfastang401@naver.com>
2023-02-17 12:02:16 +0900

    Feat : quizController 기능 수정

--- src/main/java/com/starters/ityogurt/controller/QuizController.java
+++ src/main/java/com/starters/ityogurt/controller/QuizController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.starters.ityogurt.dao.QuizDAO;
@@ -40,8 +41,69 @@ public class QuizController {
 		return mv;
 	}
 	
-	@GetMapping("/answer") 
-	public ModelAndView answer(int knowSeq, HttpServletRequest request, int radio1, int radio2,
+	
+//	@PostMapping("/answer") 
+//	public ModelAndView answer(int knowSeq, HttpServletRequest request, int radio1, int radio2,
+//			int radio3) {
+//		ModelAndView mv = new ModelAndView();
+//		//quiz번호들 저장하기
+//		String[] s1 = request.getParameterValues("quizSeq");
+//		int[] quizSeq = new int[s1.length]; 
+//		for(int i = 0;i<quizSeq.length;i++) {
+//			quizSeq[i]=Integer.parseInt(s1[i]);
+//		}
+//		
+//		//정답 가져오기
+//		int[] userChoice = {radio1,radio2,radio3};
+//		int[] answer = new int[3];
+//		for(int i=0;i<userChoice.length;i++) {
+//			answer[i] = service.getAnswer(quizSeq[i]);			
+//		}
+//		
+//		//user 입력 답이랑 정답 비교해서 정답이면 1, 오답이면 0
+//		int[] isRight = new int[3];
+//		for(int i=0;i<isRight.length;i++) {
+//			if(userChoice[i] == answer[i]) {
+//				isRight[i] = 1;						
+//				System.out.println(isRight[i]);
+//			}else {
+//				isRight[i] = 0;
+//				System.out.println(isRight[i]);
+//			}
+//		}
+//		
+//		//leanrRecord 테이블 insert
+//		int userSeq = Integer.parseInt(request.getParameter("userSeq"));
+//		for(int i=0;i<userChoice.length;i++) { //일단 나중에 다시 구현 
+//			learnRecordService.learnData(userChoice[i], isRight[i], userSeq, quizSeq[i]);
+//		}
+//		
+//		//답 보여줘야 하니 learn_record 불러오기
+//		List<LearnRecordDTO> learnList = learnRecordService.getLearn(quizSeq[0],quizSeq[1],quizSeq[2]);
+//		for(LearnRecordDTO d:learnList) {
+//			System.out.println(d);
+//		}
+//		//퀴즈 내용 불러와야 하니까 리스트 가져옴
+//		List<QuizDTO> quizList = service.quiz(knowSeq);
+//		
+//		mv.addObject("quizList", quizList);
+//		mv.addObject("learnList", learnList);
+//		request.setAttribute("quizList", quizList);
+//		
+//		
+//		mv.setViewName("redirect:/answerResult");
+//		return mv;
+//	}
+	
+//	@GetMapping("/answerResult")
+//	public String answerResult() {
+//		return "quiz/answer";
+//	}
+	
+	
+	
+	@PostMapping("/answer") 
+	public String answer(int knowSeq, HttpServletRequest request, int radio1, int radio2,
 			int radio3) {
 		ModelAndView mv = new ModelAndView();
 		//quiz번호들 저장하기
@@ -76,6 +138,15 @@ public class QuizController {
 			learnRecordService.learnData(userChoice[i], isRight[i], userSeq, quizSeq[i]);
 		}
 		
+		
+		
+		return "redirect:/answerResult?quizSeq="+quizSeq[0]+"&quizSeq="+quizSeq[1]+"&quizSeq="+quizSeq[2]+"&knowSeq="+knowSeq;
+	}
+
+	
+	@GetMapping("/answerResult")
+	public ModelAndView answerResult(int[] quizSeq, int knowSeq) {
+		ModelAndView mv = new ModelAndView();
 		//답 보여줘야 하니 learn_record 불러오기
 		List<LearnRecordDTO> learnList = learnRecordService.getLearn(quizSeq[0],quizSeq[1],quizSeq[2]);
 		for(LearnRecordDTO d:learnList) {
@@ -86,33 +157,13 @@ public class QuizController {
 		
 		mv.addObject("quizList", quizList);
 		mv.addObject("learnList", learnList);
-		request.setAttribute("quizList", quizList);
-		
-		
-		
-//		String[] chk1 = request.getParameterValues("chk");
-//		int[] chk = new int[chk1.length]; 
-		
-//		for(int i = 0;i<quizSeq.length;i++) {
-//			quizSeq[i]=Integer.parseInt(s1[i]);
-//		}
-//		for(int i:chk) {
-//			System.out.println("선택한 보기 값: "+i);
-//		}
-//	
-//		for(int a : quizSeq) {
-//			System.out.println(a);
-//		}
-		
-		
-//		System.out.println(quizSeq);
-//		System.out.println(userSeq);
-//		for(int a : check) {
-//			System.out.println(a);
-//		}
+	//	request.setAttribute("quizList", quizList);
 		
 		mv.setViewName("quiz/answer");
 		return mv;
 	}
+	
+	
+	
 	
 }
